bf80866206d018f51e95d800728c52debf1e6204
Harshwardhansjadhav <harshplugin@gmail.com>
2025-08-28 18:28:55 +0530

    send txt using python

--- send-txt-msg.py
+++ send-txt-msg.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Your Twilio account SID and Auth Token
-account_sid = 'ACcddacf7b0605d57eaadd37675482c7d3'
-auth_token = '3975ad2b79088bebb9f5ce760299c698'
+account_sid = ' your account_sid of twilio'
+auth_token = ' twilio auth token '
 
 # Create a client to interact with the Twilio API
 client = Client(account_sid, auth_token)
